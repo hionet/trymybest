ffce98505448fb379425d58ca744a8606aa875d1
ducdv <ducdv@pixelz.com>
2019-07-30 19:07:35 +0700

    trymybest-csharp: trigger-build #6

--- csharp/sources/NetCore.ConsoleApp/src/NetCore.ConsoleApp/Program.cs
+++ csharp/sources/NetCore.ConsoleApp/src/NetCore.ConsoleApp/Program.cs
@@ -6,7 +6,7 @@ namespace NetCore.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! #1");
         }
     }
 }
